a3088df8e8895a13f85728a1c46c2848da9d7380
Martin Kealey <from-github@kurahaupo.gen.nz>
2023-05-08 07:52:13 +1000

    Update kbinsert.c

--- kbinsert.c
+++ kbinsert.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <termios.h>
+#include <sysexits.h>
 
-/* Coded by Chat GPT4 (without any modifications by me, actually) */
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <characters>\n", argv[0]);
-        return 1;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s INPUTTEXT\n", argv[0]);
+        return EX_USAGE;
     }
 
     int fd = open("/dev/tty", O_RDWR);
     if (fd == -1) {
-        perror("open");
-        return 1;
+        perror("re-open controlling terminal");
+        return EX_UNAVAIL;
     }
 
-    for (size_t i = 0; i < strlen(argv[1]); i++) {
-        if (ioctl(fd, TIOCSTI, &argv[1][i]) == -1) {
-            perror("ioctl");
-            close(fd);
+    for (char *p = argv[1] ; *p ; ++p)
+        if (ioctl(fd, TIOCSTI, p) == -1) {
+            perror("ioctl(TIOCSTI)");
             return 1;
         }
-    }
 
-    close(fd);
     return 0;
 }
